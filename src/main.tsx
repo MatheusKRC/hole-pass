88d74db53e32954580cd46bce108cc0a54e8af93
Matheus leão <kyracraft0@gmail.com>
2023-11-21 17:09:47 -0300

    Criação de pastas para os proximos arquivos

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home.tsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" Component={ Home } />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 );
