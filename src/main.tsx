73526f03a3bd87f7fdc5acc781a94f6c6d151fcc
Matheus leão <kyracraft0@gmail.com>
2023-11-23 16:15:20 -0300

    style: Added Some style settigns

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home.tsx';
+import './CSS/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
