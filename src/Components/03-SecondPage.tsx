afa0f5894a858fe4753633c5c63e1de3c401788b
Matheus leão <kyracraft0@gmail.com>
2024-01-04 17:45:58 -0300

    style: positions, fonts, colors and sizes in secondPage

--- src/Components/03-SecondPage.tsx
+++ src/Components/03-SecondPage.tsx
@@ -1,4 +1,3 @@
-import starWars from '../Images/Design sem nome (4).png';
 import cloud from '../Assets/cloud.png';
 import calendar from '../Assets/calendar-minus.png';
 import logo from '../Assets/hole pass 4.png';
@@ -7,15 +6,12 @@ import future from '../Assets/Future.png';
 function SecondPage() {
   return (
     <div className="secondPage">
-      <h1 className="titlePage">PRESENCIE EVENTOS FUTUROS ANTECIPADAMENTE</h1>
+      <div className="future">
+        <h1 className="titlePage">PRESENCIE EVENTOS FUTUROS ANTECIPADAMENTE</h1>
 
-      <img className="portal" src={ future } alt="Portal para the elder scrolls VI" />
+        <img className="portal" src={ future } alt="Portal para the elder scrolls VI" />
 
-      <img
-        className="futureGame"
-        src={ starWars }
-        alt="Imagem do jogo star wars outlaws"
-      />
+      </div>
 
       <section className="benefits">
         <img className="benefitsIcon" src={ cloud } alt="Icone de uma nuvem amarela" />
@@ -29,7 +25,11 @@ function SecondPage() {
 
         </p>
 
-        <img className="benefitsIcon" src={ logo } alt="Icone de um buraco negro" />
+        <img
+          className="benefitsIconB"
+          src={ logo }
+          alt="Icone de um buraco negro"
+        />
         <p className="benefitsDesc">
           Novos jogos adicionados regularmente, desde antigos lançamentos,
           atuais e acesso antecipado aos futuros
