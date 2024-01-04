1494875453843c03c03db359fa2e96a30664ae0b
Matheus leão <kyracraft0@gmail.com>
2024-01-04 13:32:15 -0300

    feat: Pix Method implemented

--- src/Components/12-PaymentComponent.tsx
+++ src/Components/12-PaymentComponent.tsx
@@ -1,6 +1,17 @@
-import { useState } from 'react';
+/* eslint-disable @typescript-eslint/naming-convention */
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { apiKey } from '../Utils/credenciais';
 
 function PaymentComponent({ planGames }:any) {
+  const localUser:any = localStorage.getItem('blackHoleUser');
+  const user = JSON.parse(localUser);
+
+  const [qrCode, setQrCode] = useState('');
+  const [pixId, setId] = useState('');
+  const [copy, setCopy] = useState('');
+  const [status, setStatus] = useState('');
   const [type, setType] = useState('pix');
   const [card, setCard] = useState({
     name: '',
@@ -9,6 +20,58 @@ function PaymentComponent({ planGames }:any) {
     aa: '',
     cvv: '',
   });
+
+  const navigate = useNavigate();
+
+  const api = axios.create({
+    baseURL: 'https://api.mercadopago.com',
+  });
+
+  api.interceptors.request.use(async (config) => {
+    // eslint-disable-next-line max-len
+    const token = apiKey;
+    config.headers.Authorization = `Bearer ${token}`;
+
+    return config;
+  });
+
+  const { planValue } = user;
+  const number = planValue.replace('R$', '');
+
+  const setPix = async () => {
+    const body = {
+      transaction_amount: parseFloat(number),
+      description: user.plaName,
+      payment_method_id: 'pix',
+      payer: {
+        email: user.email,
+        first_name: user.firstName,
+        last_name: user.lastName,
+      },
+    };
+    api.post('v1/payments', body).then((response) => {
+      const {
+        qr_code, qr_code_base64 } = response.data.point_of_interaction.transaction_data;
+      setId(response.data.id);
+      setQrCode(qr_code_base64);
+      setCopy(qr_code);
+    }).catch((err) => {
+      // eslint-disable-next-line no-alert
+      alert(err);
+    });
+  };
+
+  const verifyStatus = () => {
+    console.log('test');
+
+    if (pixId) {
+      api.get(`v1/payments/${pixId}`).then((response) => {
+        const paymentStatus = response.data.status;
+        setStatus(paymentStatus);
+      });
+    }
+    return status;
+  };
 
   const handleClick = ({ target }:any) => {
     const { value } = target;
@@ -22,6 +85,19 @@ function PaymentComponent({ planGames }:any) {
       [id]: value,
     });
   };
+
+  useEffect(() => {
+    setPix();
+  }, []);
+
+  useEffect(() => {
+    if (status === 'approved') {
+      return navigate('/Final');
+    } return console.log('Aguardando Pagamento');
+  }, [status]);
+
+  setInterval(verifyStatus, 3000 * 10);
+
   return (
     <div>
       <h1>FORMA DE PAGAMENTO</h1>
@@ -29,7 +105,13 @@ function PaymentComponent({ planGames }:any) {
       <button onClick={ handleClick } value="pix">Pix</button>
       <button onClick={ handleClick } value="cartão">Cartão</button>
 
-      {type === 'pix' ? (<img alt="qrCode" />) : (
+      {type === 'pix' ? (
+        <div>
+          <img width="200px" alt="qrCode" src={ `data:image/jpeg;base64,${qrCode}` } />
+          <input type="text" onChange={ handleChange } value={ copy } />
+        </div>
+
+      ) : (
         <div>
           <input
             onChange={ handleChange }
@@ -40,6 +122,7 @@ function PaymentComponent({ planGames }:any) {
           <input
             onChange={ handleChange }
             id="number"
+            value=""
             placeholder="Número do Cartão"
             type="text"
           />
