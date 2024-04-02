7f2e71d9b0adea6dd14e6a69b980b914f42bc595
Matheus leão <kyracraft0@gmail.com>
2024-04-02 14:34:16 -0300

    fix jsx

--- src/Components/09-NewGames.jsx
+++ src/Components/09-NewGames.jsx
@@ -1,13 +1,14 @@
-import { NewGamesI } from '../Interfaces/gamePage';
+/* eslint-disable react/jsx-curly-spacing */
+// import { NewGamesI } from '../Interfaces/gamePage';
 
-function NewGames({ game, classname, gameName }: NewGamesI) {
+function NewGames({ game, classname, gameName }) {
   return (
-    <div data-testid="new-games" className={ `slide ${classname}` }>
+    <div data-testid="new-games" className={`slide ${classname}`}>
       <img
         data-testid="slider-background"
         className="sliderBackground"
-        src={ game }
-        alt={ gameName }
+        src={game}
+        alt={gameName}
       />
 
     </div>
