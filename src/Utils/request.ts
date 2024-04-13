1af22209c0eeac37996b6665a0eeab424a43c6e0
Matheus leão <kyracraft0@gmail.com>
2024-04-13 11:14:33 -0300

    feat: change baseUrl

--- src/Utils/request.ts
+++ src/Utils/request.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const request = axios.create({
-  baseURL: 'http://localhost:3003',
+  baseURL: 'https://holepass.netlify.app',
 });
 
 export const getData = async (endpoint:string) => {
